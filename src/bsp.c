1c89c14e99ab1c8f126b0532e205776642479bf1
Jacko Dirks <jacko.dirks@gmail.com>
2024-01-02 18:38:22 +0100

    bsp: Fix warnings and indentation

--- src/bsp.c
+++ src/bsp.c
@@ -1,37 +1,52 @@
 #include <sys/stat.h>
 
 int _fstat(int file, struct stat *st) {
-  st->st_mode = S_IFCHR;
-  return 0;
+    (void)file;
+    st->st_mode = S_IFCHR;
+    return 0;
 }
 
 int _lseek(int file, int offset, int whence) {
-  return 0;
+    (void)file;
+    (void)offset;
+    (void)whence;
+    return 0;
 }
 
 int _close(int fd) {
-  return -1;
+    (void)fd;
+    return -1;
 }
 int _write (int fd, char *buf, int count) {
-  return 0;
+    (void)fd;
+    (void)buf;
+    (void)count;
+    return 0;
 }
 
 int _read (int fd, char *buf, int count) {
-  return 0;
+    (void)fd;
+    (void)buf;
+    (void)count;
+    return 0;
 }
 
 int _isatty(int file) {
-  return 1;
+    (void)file;
+    return 1;
 }
 
 void _exit(int status) {
-    return;
+    (void)status;
+    while(1);
 }
 
 void _kill(int pid, int sig) {
-  return;
+    (void)pid;
+    (void)sig;
+    return;
 }
 
 int _getpid(void) {
-  return -1;
+    return -1;
 }
