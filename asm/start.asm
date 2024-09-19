d8307df99a3a39020f64b848cd6792b054fdfe30
Jacko Dirks <jacko.dirks@gmail.com>
2024-09-19 12:00:03 +0200

    Get rid of startup.c
    Expand __start instead

--- asm/start.asm
+++ asm/start.asm
@@ -2,11 +2,26 @@
 .global __start
 
 __start:
-        # Load stackpointer
-        lui     sp,%hi(_stack_start)
-        addi    sp,sp,%lo(_stack_start)
-        # Load global pointer
-        lui     gp,%hi(_global_pointer)
-        addi    gp,gp,%lo(_global_pointer)
-        # Jump to the startup function, which is a noreturn
-        j startup
+.option push
+.option norelax
+    # Load stackpointer
+    la      sp, _stack_start
+    # Load global pointer
+    la      gp, __global_pointer$
+.option pop
+    # Clear the bss segment
+    la      a0, _bss
+    la      a2, _ebss
+    sub     a2, a2, a0
+    li      a1, 0
+    call    memset
+    # Copy the data segment
+    la      a0, _data
+    la      a1, _flash_data
+    la      a2, _edata
+    sub     a2, a2, a0
+    call    memcpy
+    # Jump to the startup function, which is a noreturn
+    call main
+l1:
+    j l1
