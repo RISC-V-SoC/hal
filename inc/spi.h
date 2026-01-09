eb4d3e752fa22be35cc46ef9614dc4c8543edbe0
Jacko Dirks <jacko.dirks@gmail.com>
2026-01-09 22:39:04 +0100

    spi.{c|h}: txBuf should be const

--- inc/spi.h
+++ inc/spi.h
@@ -10,7 +10,7 @@ extern "C" {
 
 void spi_init(uint32_t speed_hz, bool cpol, bool cpha);
 
-void spi_transaction(uint8_t* txBuf, size_t txBufLen, uint8_t* rxBuf, size_t rxBufLen);
+void spi_transaction(const uint8_t* txBuf, size_t txBufLen, uint8_t* rxBuf, size_t rxBufLen);
 
 #ifdef __cplusplus
 }
